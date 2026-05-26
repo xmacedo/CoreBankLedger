cb3d365750883ac8e4a698d7d2f77a95e4767d7b
Felipe Macedo <felipe.x.macedo@gmail.com>
2026-05-26 18:00:51 -0300

    Adding initial structure

---
+++ src/main/java/com/xmacedo/corebankledger/CoreBankLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.xmacedo.corebankledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreBankLedgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreBankLedgerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xmacedo/corebankledger/CoreBankLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmacedo.corebankledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBankLedgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
